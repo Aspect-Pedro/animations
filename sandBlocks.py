5cdf5bc3c4d6abe57ce67512cf2b23138a9d382e
Pedro Augusto <98715404+pedroacamargo@users.noreply.github.com>
2024-01-17 16:13:31 +0000

    Procedural Generation: Sand Blocks

---
+++ sandBlocks.py
@@ -0,0 +1,14 @@
+from manim import *
+
+PATH = 'C:\\Users\Pedro\Desktop\manimAnimations\images'
+
+class Animate(Scene):
+    def construct(self):
+        sand = ImageMobject(f"{PATH}\\SandNew.png").move_to([-0.7, 0, 0]);
+        sand.height = 0.7
+        number = always_redraw(lambda: Text("7", font="Fira Code").next_to(sand,RIGHT));
+        self.play(DrawBorderThenFill(number), FadeIn(sand));
+        self.play(sand.animate.move_to([-4, 0, 0]), run_time=2)
+        zeros = Text("000 000 000 000 000",font="Fira Code").next_to(number,RIGHT,buff=0.4)
+        self.play(DrawBorderThenFill(zeros))
+        self.wait()
